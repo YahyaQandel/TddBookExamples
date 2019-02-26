e7e334af97459626c89b17eaf59931641ec55c7a
YahyaQandel <yahya.qandel@gmail.com>
2019-02-27 00:30:15 +0200

    Second fix: green the test using most baby steps

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,14 +1,15 @@
 public class Dollar {
 
+    int amount = 0;
     Dollar(int amount){
-
+        this.amount = amount;
     }
 
     void times(int multiplier){
-
+        this.amount*=multiplier;
     }
 
     int amount(){
-        return 0;
+        return amount;
     }
 }
